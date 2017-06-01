14d97c279cd62d10780607bc631955902c5f170a
Felix Bachmann <f.bachmann@silicon-computer.de>
2017-06-01 13:28:19 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,71 @@
+let config  = require('./config/config.json');
+let path    = require('path');
+let discord = require('discord.js-commando');
+let client  = new discord.Client({
+	owner: config.discord.ownerId,
+	commandPrefix: config.discord.commandPrefix
+});
+
+client.registry
+    .registerGroups(config.discord.commandGroups)
+    .registerDefaults()
+    .registerCommandsIn(path.join(__dirname, 'app/command'));
+
+client.on('error', console.error);
+client.on('warn', console.warn);
+client.on('debug', console.log);
+
+client.on('ready', () => {
+	console.log('I am ready!');
+});
+
+client.on('message', message => {
+});
+
+client.on('commandError', (cmd, err) => {
+	if (err instanceof commando.FriendlyError) return;
+	console.error('Error in command ' + cmd.groupID + ':' + cmd.memberName, err);
+});
+
+client.on('commandBlocked', (msg, reason) => {
+	console.log('Command [' + msg.command.groupID + ':' + msg.command.memberName + "] " + blocked + ". Reason: " + reason);
+});
+
+client.on('commandPrefixChange', (guild, prefix) => {
+	if (prefix === '') {
+		console.log('Prefix removed in guild ' + guild.name + '(' + guild.id + ')');
+	}
+	else {
+		console.log('Prefix changed to ' + prefix);
+	}
+});
+
+client.on('commandStatusChange', (guild, command, enabled) => {
+	var consoleMessage = 'Command ' + command.groupID + ':' + command.memberName + ' ';
+
+	if (enabled) {
+		consoleMessage += 'enabled';
+	}
+	else {
+		consoleMessage += 'disabled';
+	}
+
+	consoleMessage += ' in guild ' + guild.name + ' (' + guild.id + ')';
+	console.log(consoleMessage);
+});
+
+client.on('groupStatusChange', (guild, group, enabled) => {
+	var consoleMessage = 'Group ' + group.id + ' ';
+
+	if (enabled) {
+		consoleMessage += 'enabled';
+	}
+	else {
+		consoleMessage += 'disabled';
+	}
+
+	consoleMessage += ' in guild ' + guild.name + ' (' + guild.id + ')';
+	console.log(consoleMessage);
+});
+
+client.login(config.discord.botToken);
