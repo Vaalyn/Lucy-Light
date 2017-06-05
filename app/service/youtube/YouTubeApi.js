15845369f991f8927966a14399ac09897aa703b4
Vaalyn <vaalyndev@gmail.com>
2017-06-05 18:34:06 +0200

    Updated axis and removed unnecessary console.log in YouTubeApi

--- app/service/youtube/YouTubeApi.js
+++ app/service/youtube/YouTubeApi.js
@@ -4,7 +4,6 @@ let logger = app.logger;
 
 module.exports = class YouTubeApi {
 	static getYouTubeVideo(title, artist) {
-		console.log(app);
 		return new Promise(function(resolve, reject) {
 			axios.get('https://www.googleapis.com/youtube/v3/search?part=id&q=' + artist + ' ' + title + '&maxResults=1&type=video&key=' + app.config.google.apiKey)
 				.then((response) => {
