e4f964c858ce3b88e979ec37cc09c5c1bd04b7da
Vaalyn <vaalyndev@gmail.com>
2017-12-01 19:22:08 +0100

    Added check to prevent users from getting bits by occasionally spamming the same message

--- app/listener/message/coins/CoinsMessageListener.js
+++ app/listener/message/coins/CoinsMessageListener.js
@@ -6,6 +6,7 @@ let logger = app.logger;
 module.exports = class CoinsMessageListener {
 	constructor() {
 		this.users = [];
+		this.usersLastMessages = [];
 	}
 
 	parseMessage(message) {
@@ -19,15 +20,30 @@ module.exports = class CoinsMessageListener {
 
 		let userId = message.author.id;
 		let username = message.author.tag;
-		let timeSinceLastEvent = this.timeSinceLastCoinEventForUser(userId);
 
-		if (timeSinceLastEvent > app.config.listener.message.coins.timeBetweenEvents) {
+		if (this.hasEnoughTimePassed(userId) && !this.isMessageSpam(message)) {
 			this.users[userId] = moment();
 
 			let coins = Math.floor(Math.random() * 5) + 1;
 
 			app.services.brg.addCommunityUserCoins(username, userId, coins);
 		}
+
+		this.usersLastMessages[userId].unshift(message.content);
+
+		if (this.usersLastMessages[userId].length > 5) {
+			this.usersLastMessages[userId].pop();
+		}
+	}
+
+	hasEnoughTimePassed(userId) {
+		let timeSinceLastEvent = this.timeSinceLastCoinEventForUser(userId);
+
+		if (timeSinceLastEvent > app.config.listener.message.coins.timeBetweenEvents) {
+			return true;
+		}
+
+		return false;
 	}
 
 	timeSinceLastCoinEventForUser(userId) {
@@ -36,5 +52,21 @@ module.exports = class CoinsMessageListener {
 		}
 
 		return Number.MAX_VALUE;
+	}
+
+	isMessageSpam(message) {
+		let isSpam = false;
+
+		if (this.usersLastMessages[message.author.id] === undefined) {
+			this.usersLastMessages[message.author.id] = [];
+		}
+
+		this.usersLastMessages[message.author.id].forEach((userMessage) => {
+			if (message.content === userMessage) {
+				isSpam = true;
+			}
+		});
+
+		return isSpam;
 	}
 }
